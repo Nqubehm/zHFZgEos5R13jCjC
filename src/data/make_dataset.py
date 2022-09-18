3e697c237e07545550f2bdd3008501db515ba3af
Lungile Mngonyama <gamedzenonny@gmail.com>
2022-09-18 20:00:26 +0200

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,8 +1,5 @@
 # Imports
-import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Import data
-data = pd.read_csv("")
+data = pd.read_csv("term-deposit-marketing-2020.csv")
