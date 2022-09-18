22e86b31c52c6ccdb8b5e26cf347ca05232d614d
Lungile Mngonyama <gamedzenonny@gmail.com>
2022-09-18 19:56:48 +0200

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,29 +1,8 @@
 # Imports
-from sklearn.preprocessing import LabelEncoder
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Check null counts in all our features
-data.isnull().sum()
-
-# Confirm the new data size
-data.shape
-
-# Remove duplicates if any
-data.drop_duplicates(inplace=True)
-
-#### Convert Categorical To Numerical Data
-
-# Get only categorical features
-cat_data = data.select_dtypes(exclude='int64')
-
-# Exclude the dependent variable
-cat_data = cat_data.drop('y', axis=1)
-
-# Instantiate label encoder
-encoder = LabelEncoder()
-
-# encode all categorical to numericals
-for i in cat_data.columns:
-    data[i] = encoder.fit_transform(data[i])
-    
-# Check our new data 
-data.head()   
+# Import data
+data = pd.read_csv("")
