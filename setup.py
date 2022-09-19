1769ad0680affcda719299d97ce863a2c6bb3464
Lungile Mngonyama <gamedzenonny@gmail.com>
2022-09-19 19:19:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
 
+setup(
+  name='lm-term-deposit-marketing-analysis',
+  version='1.0.0',
+  description='Machine learning model that better predicts the customers that are more likely to buy the investment product from a startup in the European banking market',
+  author='Lungile Mngonyama',
+  author_email='lungile1201@gmail.com',
+  url='https://github.com/Nqubehm/zHFZgEos5R13jCjC',
+  packages=find_packages()
+)
